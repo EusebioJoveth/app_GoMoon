51b079be7b15c5c8c178b2ba0cb8616649b355cb
Eusebio Joveth <yurijoveth@gmail.com>
2023-12-21 15:48:23 +0100

    Access Device Height and Width

--- lib/pages/home_pages.dart
+++ lib/pages/home_pages.dart
@@ -1,19 +1,39 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  late double _deviceHeigth, _deviceWidth;
 
+  HomePage({super.key});
   @override
   Widget build(BuildContext context) {
+    _deviceHeigth = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-            color: Colors.red,
-            image: DecorationImage(
-              fit: BoxFit.fill,
-              image: AssetImage("assets/images/astro.png"),
-            )),
+        body: SafeArea(
+      child: Container(
+        height: _deviceHeigth,
+        width: _deviceWidth,
+        padding: EdgeInsets.symmetric(horizontal: _deviceWidth * 0.05),
+        child: _pageTitle(),
       ),
+    ));
+  }
+
+  Widget _pageTitle() {
+    return const Text(
+      '#VouAoEspaço',
+      style: TextStyle(
+          color: Colors.white, fontSize: 35, fontWeight: FontWeight.w800),
+    );
+  }
+
+  Widget _astroImageWidget() {
+    return Container(
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+        fit: BoxFit.fill,
+        image: AssetImage("assets/images/astro.jpg"),
+      )),
     );
   }
 }
