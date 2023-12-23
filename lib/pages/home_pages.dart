3c1c787a10275d44bbd3b88f06bcf36432dde333
Eusebio Joveth <yurijoveth@gmail.com>
2023-12-23 16:15:29 +0100

    Stack and Alignment Widget

--- lib/pages/home_pages.dart
+++ lib/pages/home_pages.dart
@@ -15,14 +15,21 @@ class HomePage extends StatelessWidget {
         height: _deviceHeigth,
         width: _deviceWidth,
         padding: EdgeInsets.symmetric(horizontal: _deviceWidth * 0.05),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Stack(
           children: [
-            _pageTitle(),
-            _destinationDropDownWidget(),
-            _travellersInformationWidget(),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _pageTitle(),
+                _bookRideWidget(),
+              ],
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: _astroImageWidget(),
+            ),
           ],
         ),
       ),
@@ -39,11 +46,29 @@ class HomePage extends StatelessWidget {
 
   Widget _astroImageWidget() {
     return Container(
+      height: _deviceHeigth * 0.50,
+      width: _deviceWidth * 0.45,
       decoration: const BoxDecoration(
           image: DecorationImage(
         fit: BoxFit.fill,
         image: AssetImage("assets/images/astro.jpg"),
       )),
+    );
+  }
+
+  Widget _bookRideWidget() {
+    return Container(
+      height: _deviceHeigth * 0.25,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _destinationDropDownWidget(),
+          _travellersInformationWidget(),
+          _rideButton(),
+        ],
+      ),
     );
   }
 
@@ -54,9 +79,42 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _travellersInformationWidget() {
-    return CustomDropDownButtonClass(
-      values: const ['1', '2', '3', '4'],
-      width: _deviceWidth * 0.45,
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        CustomDropDownButtonClass(
+          values: const ['1', '2', '3', '4'],
+          width: _deviceWidth * 0.45,
+        ),
+        CustomDropDownButtonClass(
+          values: const ['Economia', 'Negócios', 'Talento', 'Investigação'],
+          width: _deviceWidth * 0.40,
+        ),
+      ],
+    );
+  }
+
+  Widget _rideButton() {
+    return Container(
+      margin: EdgeInsets.only(
+        bottom: _deviceHeigth * 0.01,
+      ),
+      width: _deviceWidth,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: MaterialButton(
+        onPressed: () {},
+        child: const Text(
+          "Passeio Livre",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+      ),
     );
   }
 }
